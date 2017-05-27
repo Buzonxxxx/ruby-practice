170348a11816fbc200a38bd90ea3cc6b10f3418d
Louis Liao <louisliao@kkstream.com.tw>
2017-05-28 01:22:49 +0800

    Implement my blog <Selfweb.html>

--- SelfProject/sololearn.rb
+++ SelfProject/sololearn.rb
@@ -1,24 +1,24 @@
-# text = "I am learning Ruby and it is fun!"
-# text.downcase!
-# freqs = {}
-# freqs.default = 0
+text = "I am learning Ruby and it is fun!"
+text.downcase!
+freqs = {}
+freqs.default = 0
 
-# text.each_char do |char| 
-# 	freqs[char] += 1
+text.each_char do |char| 
+	freqs[char] += 1
 
-# end
-# p freqs
-# ("a".."z").each do |x| 
-# 	puts "#{x} : #{freqs[x]}" 
-# end
+end
+p freqs
+("a".."z").each do |x| 
+	puts "#{x} : #{freqs[x]}" 
+end
 
 
-# h = {a:2, b:8, c:73}
-# sum = 0
-# h.each do |k, v|
-#   sum+=v
-# end
-# p sum
+h = {a:2, b:8, c:73}
+sum = 0
+h.each do |k, v|
+  sum+=v
+end
+p sum
 
 def greet(name="")
   if name==""
